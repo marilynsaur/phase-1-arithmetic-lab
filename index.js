2229aedfce5b79e2747998c59474545fbefe9f6b
Marilyn Wren <Marilynwren64@yahoo.com>
2022-02-02 11:03:17 -0500

    updat

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+function multiply() {
+    let num1 = 2
+    let num2 = 31
+
+    return num1 *= num2;
+}
+console.log(multiply(2));
+
+function random(integer) {
+    return Math.floor(Math.random() * integer);
+}
+console.log(random(3));
+
+function mod() {
+    let num3 = 12
+    let num4 = 3
+    return num3 /= num4;
+}
+console.log(mod(12));
+
+function max() {
+    return Math.max(1, 2, 3, 4, 20);
+}
+console.log(max());
