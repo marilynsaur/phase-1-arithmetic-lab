aceb2ab8b659947d60570d8eeaf7b19219c9fa7b
Marilyn Wren <Marilynwren64@yahoo.com>
2022-02-02 11:23:48 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 
 // Write your code here
 
-function multiply() {
-    let num1 = 2
-    let num2 = 31
 
-    return num1 *= num2;
-}
-console.log(multiply(2));
+    const num1 = 2
+    const num2 = 31
+    const num4 = 12
+    const num3 = 4
 
-function random(integer) {
-    return Math.floor(Math.random() * integer);
-}
-console.log(random(3));
+    const multiply= num1 * num2;
 
-function mod() {
-    let num3 = 12
-    let num4 = 3
-    return num3 /= num4;
-}
-console.log(mod(12));
 
-function max() {
-    return Math.max(1, 2, 3, 4, 20);
-}
-console.log(max());
+
+    const random = Math.floor(Math.random() * 8);
+
+
+
+
+    
+    const mod= num3 % num4;
+
+
+
+
+    const max= Math.max(1, 2, 3, 4, 20);
+
